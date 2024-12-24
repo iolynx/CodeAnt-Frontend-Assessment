2c0b8d721865f315ab00b3949d907bb5865ede0a
Vishal <santaclawsers@gmail.com>
2024-12-24 22:43:15 +0530

    added phone navbar + drawer

--- src/components/PhoneHeader.jsx
+++ src/components/PhoneHeader.jsx
@@ -1,21 +1,54 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Stack, IconButton, styled } from "@mui/material";
+import MuiDrawer, { drawerClasses } from '@mui/material/Drawer'
 import CodeAntLogo from '../assets/CodeAnt.svg?react'
+import MenuIcon from '../assets/iconsvg/menu.svg?react'
+import PhoneMenu from './PhoneMenu.jsx'
+import { useState } from 'react'
+
+const drawerWidth = 'fullWidth';
+
+const Drawer = styled(MuiDrawer)({
+  width: drawerWidth,
+  padding: "10px",
+  flexShrink: 0,
+  boxSizing: 'border-box',
+  mt: 10,
+  textAlign: 'left',
+  [`& .${drawerClasses.paper}`]: {
+    width: drawerWidth,
+    boxSizing: 'border-box',
+  },
+});
 
 export default function PhoneHeader() {
+  const [open, setOpen] = useState(false)
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen)
+  }
+
+
+
   return (
     <AppBar position='static'
       sx={{
         display: { md: 'none', xs: 'flex' },
         backgroundColor: '#ffffff',
         height: '60px',
+        width: '90%',
         boxShadow: 'none',
         borderBottom: '1px solid var(--template-palette-divider)',
         mt: 2,
         mx: 2,
-        justifyContent: 'space-between'
       }}
     >
-      <CodeAntLogo />
+      <Stack direction='row' sx={{ justifyContent: 'space-between' }}>
+        <CodeAntLogo />
+        <IconButton outline='none' onClick={toggleDrawer(true)}> <MenuIcon /> </IconButton>
+        <Drawer open={open} onClose={toggleDrawer(false)} anchor='top'>
+          <PhoneMenu toggleDrawer={toggleDrawer} />
+        </Drawer>
+      </Stack>
     </AppBar>
   );
 }
---
+++ src/components/PhoneMenu.jsx
@@ -0,0 +1,47 @@
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { Stack, Typography, IconButton } from '@mui/material'
+import HomeIcon from '../assets/iconsvg/home.svg?react'
+import CodeIcon from '../assets/iconsvg/code.svg?react'
+import CloudIcon from '../assets/iconsvg/cloud.svg?react'
+import BookIcon from '../assets/iconsvg/book.svg?react'
+import GearIcon from '../assets/iconsvg/gear.svg?react'
+import PhoneIcon from '../assets/iconsvg/phone.svg?react'
+import SignOutIcon from '../assets/iconsvg/sign-out.svg?react'
+import CodeAntLogo from '../assets/CodeAnt.svg?react'
+import CloseIcon from '../assets/iconsvg/close.svg?react'
+
+const mainListItems = [
+  { text: 'Repositories', icon: <HomeIcon /> },
+  { text: 'AI Code Review', icon: <CodeIcon /> },
+  { text: 'Cloud Security', icon: <CloudIcon /> },
+  { text: 'How to use', icon: <BookIcon /> },
+  { text: 'Settings', icon: <GearIcon /> },
+  { text: 'Support', icon: <PhoneIcon /> },
+  { text: 'Logout', icon: <SignOutIcon /> },
+];
+
+// eslint-disable-next-line react/prop-types
+export default function PhoneMenu({ toggleDrawer }) {
+  return (
+    <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
+      <Stack direction='row' sx={{ justifyContent: 'space-between' }}>
+        <CodeAntLogo />
+        <IconButton onClick={toggleDrawer(false)}> <CloseIcon /> </IconButton>
+      </Stack>
+      <List dense>
+        {mainListItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: 'block', mb: 0.8 }}>
+            <ListItemButton selected={index === 0}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography sx={{ fontWeight: '600 !important' }}> {item.text} </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Stack>
+  )
+}
