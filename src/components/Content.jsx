2393be804ae40376264e1d629af62a84cf141b00
Vishal <santaclawsers@gmail.com>
2024-12-26 15:55:27 +0530

    more layout fixes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,9 @@
 import Statistics from './Statistics'
-import { Stack, Box } from "@mui/material";
+import { Stack } from "@mui/material";
 
 export default function Content() {
   return (
-    <Stack direction="column" sx={{ pt: 25, px: 14, minWidth: '10%', maxWidth: '110%' }} >
+    <Stack direction="column" sx={{ pt: 25, px: 14, minWidth: '10%', maxWidth: '110%', display: { xs: 'none', sm: 'none', md: 'flex' } }} >
       <Statistics />
     </Stack>
   )
